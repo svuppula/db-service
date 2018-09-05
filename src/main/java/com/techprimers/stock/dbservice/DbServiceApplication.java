fb5cfa14254d9cdc7dc5a9530e7d4437a127fe63
satish reddy vuppula <satishreddy.vuppula@gmail.com>
2018-09-04 20:22:43 -0400

    eureka configurations added

--- src/main/java/com/techprimers/stock/dbservice/DbServiceApplication.java
+++ src/main/java/com/techprimers/stock/dbservice/DbServiceApplication.java
@@ -2,11 +2,12 @@ package com.techprimers.stock.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages="com.techprimers.stock.dbservice.repository")
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class DbServiceApplication {
 
